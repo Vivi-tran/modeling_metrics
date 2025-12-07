f7ba7bbb5cd685a2b7c675362b90123119cb03b4
Vivi-tran <nguyen-ngoc-vi.tran.6279@student.uu.se>
2025-12-08 00:48:01 +0100

    MODIFY: adapt to omnibenchmark pipeline

--- metrics/correlation.py
+++ metrics/correlation.py
@@ -209,10 +209,7 @@ def main() -> None:
         features=features,
     )
 
-    if output_path.endswith(".tsv"):
-        corr_df.to_csv(args.output_dir, f"{name}.correlation.csv", sep="\t", index=False)
-    else:
-        corr_df.to_csv(args.output_dir, f"{name}.correlation.csv", index=False)
+    corr_df.to_csv(args.output_dir, f"{name}.correlation.csv", index=False)
 
 if __name__ == "__main__":
     main()
